417119e7881d1ad3379b2fe484250e82086e2482
Mingming <1609772528@qq.com>
2023-05-23 12:05:33 +0800

    Update conf.py
    更改项目信息

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ziku'
+copyright = '2023, Ruyi'
+author = 'Zhaomingming'
 
 release = '0.1'
 version = '0.1.0'
